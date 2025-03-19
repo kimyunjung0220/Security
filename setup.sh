c31df2aa14034c1edd971a61d7a2684d2ae6c5d6
kimyunjung0220 <gimyunjung4@naver.com>
2025-03-20 01:04:51 +0900

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-USER_NAME=$(whoami)
-
-sudo apt install wireshark -y
-sudo usermod -aG wireshark "$USER_NAME"
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-sudo pip3 install pyshark
-sudo pip3 install flask
