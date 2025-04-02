2bcd1ab05e5e841366a2f6ae180eaed6d128df31
kimyunjung0220 <gimyunjung4@naver.com>
2025-04-02 11:38:05 +0900

    Delete IDS_Software directory
    새로운 카테고리 설정

--- IDS_Software/find_interface.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import get_if_list
-
-def find_interface():
-    interfaces = get_if_list()
-    for interface in interfaces:
-        if interface.startswith('eth') or interface.startswith('ens'):
-            return interface
